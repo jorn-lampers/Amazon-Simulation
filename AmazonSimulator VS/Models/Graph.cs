f112479497b44d403b5898a1e20e4a3e0f38982a
Jorn <jorn@lampers-ict.nl>
2019-12-28 11:43:06 +0100

    Implemented Dijkstra's algorithm on Graph class

--- AmazonSimulator VS/Models/Graph.cs
+++ AmazonSimulator VS/Models/Graph.cs
@@ -51,10 +51,15 @@ namespace Models
         {
             if(o is Edge)
             {
-                Edge oe = (Edge)o;
+                Edge oe = (Edge) o;
                 return (oe.a.Equals(this.a) && oe.b.Equals(this.b)) || (oe.b.Equals(this.a) && oe.a.Equals(this.b));
             }
             return false;
+        }
+
+        public Boolean Contains(Vertex v)
+        {
+            return a.Equals(v) || b.Equals(v);
         }
     }
 
@@ -87,15 +92,105 @@ namespace Models
             return nearest;
         }
 
-        public LinkedList<Vertex> findShortestRoute(Vertex source, Vertex destination)
+        public List<Vertex> findAdjacentNodes(Vertex v)
         {
-            if (!_vertices.Contains(source) || !_vertices.Contains(destination))
-                throw new ArgumentException("Either one or both supplied vertices are non-existant in Graph.");
+            if (!_vertices.Contains(v))
+                throw new ArgumentException("Supplied Vertex is not in this Graph.");
 
-            LinkedList<Vertex> route = new LinkedList<Vertex>();
-            route.AddFirst(source);
+            List<Vertex> adjacents = new List<Vertex>();
 
-            return route; // TODO: Implement this
+            foreach(Edge e in _edges)
+            {
+                if(e.a.Equals(v)) adjacents.Add(e.b);
+                else if(e.b.Equals(v)) adjacents.Add(e.a);
+            }
+
+            return adjacents;
+        }
+
+        /// <summary>
+        /// Calculates inter-node distances using Dijkstra's algorithm as specified in https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+        /// </summary>
+        /// <param name="source">The source node to calculate distances from.</param>
+        /// <param name="destination">The target node to calculate distances to.</param>
+        /// <returns>A map containing distances from key node to source node and a previous node if applicable.</returns>
+        public Dictionary<Vertex, Tuple<double, Vertex>> dijkstraDists(Vertex source, Vertex destination)
+        {
+            Dictionary<Vertex, double> dist = new Dictionary<Vertex, double>();
+            Dictionary<Vertex, Vertex> prev = new Dictionary<Vertex, Vertex>();
+
+            List<Vertex> unvisited = new List<Vertex>();
+
+            foreach (Vertex v in _vertices)
+            {
+                dist[v] = Double.MaxValue;
+                unvisited.Add(v);
+                prev[v] = null;
+            }
+
+            dist[source] = 0.0; // Distance from source is always zero
+            unvisited.Remove(source);
+
+            // Find distances to nodes adjacent to source node
+            foreach(Vertex a in findAdjacentNodes(source))
+            {
+                if (a.distanceTo(source) < dist[a])
+                {
+                    dist[a] = a.distanceTo(source);
+                    prev[a] = source;
+                }
+            }
+
+            while(unvisited.Count > 0)
+            {
+                unvisited.Sort((a, b) => dist[a].CompareTo(dist[b])); // Sort unvisited ascending by distance from source node
+
+                Vertex current = unvisited[0];
+                unvisited.Remove(current);
+                foreach (Vertex a in findAdjacentNodes(current))
+                {
+                    if (!unvisited.Contains(a)) continue; // No need to update nodes that have already been visited
+                    double nDist = dist[current] + a.distanceTo(current);
+                    if (nDist < dist[a])
+                    {
+                        dist[a] = nDist; // Update distance to Node A only, and only if it is the smallest distance yet found.
+                        prev[a] = current;
+                    }
+                }
+            }
+
+            Dictionary<Vertex, Tuple<double, Vertex>> vals = new Dictionary<Vertex, Tuple<double, Vertex>>();
+            foreach(Vertex key in dist.Keys)
+            {
+                vals.Add(key, new Tuple<double, Vertex>(dist[key], prev[key]));
+            }
+
+            return vals;
+        }
+
+        /// <summary>
+        /// Finds the shortest path from Node A to B on this graph using Dijkstra's algorithm: https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+        /// </summary>
+        /// <param name="source">The starting node.</param>
+        /// <param name="destination">The target node.</param>
+        /// <returns>A list of vertices representing the shortest path from passed Source Node to Target Node.</returns>
+        public List<Vertex> dijkstraShortestPath(Vertex source, Vertex destination)
+        {
+            Dictionary<Vertex, Tuple<double, Vertex>> data = dijkstraDists(source, destination);
+
+            List<Vertex> path = new List<Vertex>();
+            path.Add(destination);
+
+            Vertex next = destination;
+            while(next != source)
+            {
+                next = data[next].Item2;
+                path.Add(next);
+            }
+
+            path.Reverse();
+
+            return path;
         }
     }
 }
--- AmazonSimulator VS/Program.cs
+++ AmazonSimulator VS/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Models;
 
 namespace AmazonSimulator_VS
 {
@@ -14,12 +15,52 @@ namespace AmazonSimulator_VS
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //BuildWebHost(args).Run();
+            testDijkstra();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+
+        public static void testDijkstra()
+        {
+            Vertex a = new Vertex(0.0, 0.0, 0.0);
+            Vertex b = new Vertex(100.0, 0.0, 0.0);
+            Vertex c = new Vertex(1.0, 0.0, 0.0);
+            Vertex d = new Vertex(8.0, 0.0, 0.0);
+            Vertex e = new Vertex(5.0, 0.0, 0.0);
+
+            Edge u = new Edge(a, b);
+            Edge v = new Edge(a, c);
+
+            Edge w = new Edge(b, d);
+            Edge x = new Edge(c, d);
+
+            Edge y = new Edge(d, e);
+
+            Vertex testV = new Vertex(6.0, 0.0, 0.0);
+            Edge testEA = new Edge(testV, a);
+            Edge testEB = new Edge(testV, e);
+
+            List<Vertex> vertices = new List<Vertex>();
+            vertices.Add(a); vertices.Add(b); vertices.Add(c); vertices.Add(d); vertices.Add(e);
+
+            List<Edge> edges = new List<Edge>();
+            edges.Add(u); edges.Add(v); edges.Add(w); edges.Add(x); edges.Add(y);
+
+            vertices.Add(testV); edges.Add(testEA); edges.Add(testEB);
+
+            Graph g = new Graph(vertices, edges);
+
+            Vertex source = a;
+            Vertex destination = e;
+
+            Dictionary<Vertex, Tuple<double, Vertex>> dist = g.dijkstraDists(source, destination);
+            List<Vertex> path = g.dijkstraShortestPath(source, destination);
+
+            Console.WriteLine(dist);
+        }
     }
 }
